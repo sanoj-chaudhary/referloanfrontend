439fd35360624d2fa7db333af3cac2c1a6e5cd5c
priyanka agarwal <priyankaagarwal@referloan.in>
2022-09-23 12:09:41 +0530

    create bank content page API

--- pages/api/[...component].js
+++ pages/api/[...component].js
@@ -39,6 +39,13 @@ export default async function handler(req, res) {
           return await getPageBySlug(req, res);
         }
       }
+      if(req.query.component[0]=='bank')
+      {
+        if(req.query.component[1])
+        {
+          return await getBankBySlug(req, res);
+        }
+      }
     default:
       return res.status(400).send("Method not allowed");
   }
@@ -77,24 +84,24 @@ const getHeaderMenu = async (req, res) => {
   {
     const results1 =  await pool.query("SELECT id,name,icon FROM `menu_types` ");
     
-    results1.forEach(function(result1)
-    {
-       const menu_id  =    result1['id'];
-       const results2 =    pool.query("SELECT * FROM `menus` LEFT JOIN `categories` ON categories.id = menus.cat_id WHERE `type1` = "+menu_id+" AND `page_id` = '0' AND `is_active` = '1' ");
+    // results1.forEach(function(result1)
+    // {
+    //    const menu_id  =    result1['id'];
+    //    const results2 =    pool.query("SELECT * FROM `menus` LEFT JOIN `categories` ON categories.id = menus.cat_id WHERE `type1` = "+menu_id+" AND `page_id` = '0' AND `is_active` = '1' ");
 
-       results2.forEach(function(result2)
-       {
-        const cat_id    =   results2['id'];
-        const results3  =   pool.query("SELECT * FROM `menus` LEFT JOIN `pages` ON pages.id = menus.page_id WHERE `type1` = "+menu_id+" AND `cat_id` = "+cat_id+" AND `is_active` = '1' ");
+      //  results2.forEach(function(result2)
+      //  {
+      //   const cat_id    =   results2['id'];
+      //   const results3  =   pool.query("SELECT * FROM `menus` LEFT JOIN `pages` ON pages.id = menus.page_id WHERE `type1` = "+menu_id+" AND `cat_id` = "+cat_id+" AND `is_active` = '1' ");
     
-        results3.forEach(function(result3)
-        {
-            const page_id    =  result3['id'];
-            const page_name  =  result3['post_title'];    
-        });
-       })
+      //   results3.forEach(function(result3)
+      //   {
+      //       const page_id    =  result3['id'];
+      //       const page_name  =  result3['post_title'];    
+      //   });
+      //  })
     
-    })
+   // })
 
     return res.status(200).json(results1);
   } 
@@ -132,6 +139,16 @@ const getCategoryBySlug = async (req, res) => {
 };
 
 const getPageBySlug = async (req, res) => {
+  try {
+    const slug = req.query.component[1];
+    const results = await pool.query("SELECT * FROM `pages` WHERE `post_slug` = '"+slug+"' ");
+    return res.status(200).json(results);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+
+const getBankBySlug = async (req, res) => {
   try {
     const slug = req.query.component[1];
     const results = await pool.query("SELECT * FROM `pages` WHERE `post_slug` = '"+slug+"' ");
