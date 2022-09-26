3c512e588aaacc1091bc747abd59b0dcb20b1dab
sanoj-chaudhary <101033913+sanoj-chaudhary@users.noreply.github.com>
2022-09-26 14:54:39 +0530

    RF-27-make-validation-standard-function-

--- public/js/common-validation.js
+++ public/js/common-validation.js
@@ -16,3 +16,10 @@ $(document).on('keypress', '.onlyalphabetsspace', function (event) {
     return false;
   }
 });
+
+let panregex = "[A-Z]{5}[0-9]{4}[A-Z]{1}";
+let panmsg = "Invalid pan number";
+let emailregex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+let emailmsg = "Invalid emai address";
+let aadhaarregex=/^[2-9]{1}[0-9]{3}\s{1}[0-9]{4}\s{1}[0-9]{4}$/;
+let aadhaarmsg = 'Invalid aadhaar number';
