a19b8b0d9d1f51eae91d0ca9a46fbb2acafaabdb
priyanka agarwal <priyankaagarwal@referloan.in>
2022-10-12 17:20:20 +0530

    changes in partner module

--- components/home/ourpartner.js
+++ components/home/ourpartner.js
@@ -1,61 +1,29 @@
-import Carousel from "react-slick";
-import React, { useEffect, useState } from "react";
+import { db } from "../../config/db";
 import Image from 'next/image'
-import axios from "axios";
 
-
-function Partner() 
-{
-  const settings = {
-    dots: false,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    speed: 2000,
-    autoplaySpeed: 2000,
-    arrows: false,
-    cssEase: "linear"
-  };
-  
-  const [data, setData] = useState([])
-
-  const getPartner = async ()=>{
-  const res = await axios.get('/api/partner');
-  const result = await res.data;
-  setData(result)
-  }
-
-  useEffect(() => {
-    getPartner()
-  }, [])
- 
+function Partner({ data }) {
 
   return (
     <section class="partnerArea">
-        <div class="container">
-            <h2 class="heading text-center">Our partners</h2>
-            <h3>from across the industry</h3>
-            <ul>
-                <li><img src="/images/ptn-logo1.png" alt="" /></li>
-                <li><img src="/images/ptn-logo2.png" alt="" /></li>
-                <li><img src="/images/ptn-logo3.png" alt="" /></li>
-                <li><img src="/images/ptn-logo4.png" alt="" /></li>
-                <li><img src="/images/ptn-logo5.png" alt="" /></li>
-                <li><img src="/images/ptn-logo1.png" alt="" /></li>
-                <li><img src="/images/ptn-logo2.png" alt="" /></li>
-                <li><img src="/images/ptn-logo3.png" alt="" /></li>
-                <li><img src="/images/ptn-logo4.png" alt="" /></li>
-                <li><img src="/images/ptn-logo5.png" alt="" /></li>
-                <li><img src="/images/ptn-logo1.png" alt="" /></li>
-                <li><img src="/images/ptn-logo2.png" alt="" /></li>
-                <li><img src="/images/ptn-logo3.png" alt="" /></li>
-                <li><img src="/images/ptn-logo4.png" alt="" /></li>
-                <li><img src="/images/ptn-logo5.png" alt="" /></li>
-            </ul>
-        </div>
+      <div class="container">
+        <h2 class="heading text-center">Our partners</h2>
+        <h3>from across the industry</h3>
+        <ul>
+          {data.map((datas) => (
+            <li>{datas.name} <Image src={'/uploads/partner/' + datas.logo_path} layout='fill' /></li>
+          )
+          )}
+        </ul>
+      </div>
     </section>
   )
 
 }
+
+export async function getServerSideProps() {
+  const res = await db.query("SELECT * FROM `partners` WHERE `is_active` = '1'")
+  const data = await res.json()
+  return { props: { data } }
+}
+
 export default Partner
