9cf51862d038c6e4227fb9938f75f84eb800f81b
priyanka agarwal <priyankaagarwal@referloan.in>
2022-10-13 14:35:35 +0530

    updates

--- components/home/ourvideo.js
+++ components/home/ourvideo.js
@@ -1,14 +1,12 @@
 
-function ourVideo({ video }) {
-
-  if (!video) return <p className="d-flex justify-content-center">No video data</p>
+function ourVideo(props) {
 
   return (
     <section className="videoSevtion">
       <h2 className="heading text-center">Our Videos</h2>
       <div className="container">
         <ul className="vdoArea">
-          {video && video.map((value, key) => (
+          {props.video && props.video.map((value, key) => (
             <li key={key}>
               <div className="videBox">
                 <iframe src={value.embedded_code}></iframe>
@@ -20,14 +18,6 @@ function ourVideo({ video }) {
       </div>
     </section>
   )
-}
-
-export async function getServerSideProps() {
-  const result = await db.query(" SELECT * FROM `videos` WHERE `is_active` = '1' ");
-  const data = result.json();
-  return {
-    props: { data }
-  }
 }
 
 export default ourVideo
