f003f9711f79ed3473cf5e9d14a3d1851aebb8ec
shubham.hydear@gmail.com <shubham.hydear@gmail.com>
2022-10-13 17:57:19 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Index({ partner,video,testimonial }) {
 
   return (
     <>
-      <Home partner={partner} video={video} testimonial={testimonial} />
+      <Home partner={partner}  testimonial={testimonial} />
     </>
   )
 
@@ -13,12 +13,11 @@ export default function Index({ partner,video,testimonial }) {
 
 export async function getServerSideProps() {
 
-  const results = await db.query("SELECT * FROM `videos` WHERE `is_active` = '1' ");
-  const video = JSON.parse(JSON.stringify(results))
+  
   const testimonialRes = await db.query("SELECT * FROM `testimonials` WHERE `is_active` = '1' ");
   const testimonial = JSON.parse(JSON.stringify(testimonialRes))
 
   const res = await db.query("SELECT logo_path,name FROM `partners` WHERE `is_active` = '1' ");
   const partner = JSON.parse(JSON.stringify(res))
-  return { props: { partner,video,testimonial } }
+  return { props: { partner,testimonial } }
 }
