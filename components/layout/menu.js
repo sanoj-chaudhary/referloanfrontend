6bdc000668dd1b187756963d9713671f3196330e
sanoj-chaudhary <101033913+sanoj-chaudhary@users.noreply.github.com>
2022-10-13 17:34:30 +0530

    RF-72-impliment-new-header-menu

--- components/layout/menu.js
+++ components/layout/menu.js
@@ -1,93 +1,146 @@
-import Link from 'next/link'
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import SubMenu from './subMenu'
+
+
 const Menu = () => {
-  const [header, setHeader] = useState([]);
+    return (
+        <header>
+           <div class="mmobile_menu">
+                <nav id="menu">
+                    <ul>
+                        <li>
+                            <a target="_blank" href="#" title="Cards"> Cards</a>
+                        </li>
+                        <li>
+                            <span>Loans</span>
+                            <ul>
+                                <li> <a class="" target="_blank" href="#" title="Personal Loan">Personal Loan</a>
+                                </li>
+                                <li><a class="" target="_blank" href="#" title="Gold Loan">Gold Loan </a></li>
+                                <li><a class="" target="_blank" href="#" title="Education Loan">Education Loan </a>
+                                </li>
+                                <li><a class="" target="_blank" href="#" title="Home Loan">Home Loan</a></li>
+                                <li><a class="" target="_blank" href="#" title="Business Loan">Business Loan</a></li>
+                                <li><a class="" target="_blank" href="#" title="Loan">Loan</a></li>
+                                {/* <!--  --> */}
+                                <li><a class="" target="_blank" href="#" title="ICICI Bank Business Loan Overdraft">ICICI
+                                    Bank
+                                    Business Loan
+                                    Overdraft</a></li>
+                                <li><a class="" target="_blank" href="#" title="Tata Capital Used Car Loan">Tata Capital
+                                    Used Car Loan</a></li>
+                                <li><a class="" target="_blank" href="#" title="Faircent Personal Loan">Faircent
+                                    Personal Loan</a></li>
+                                <li><a class="" target="_blank" href="#" title="InstaMoney Personal Loan">InstaMoney
+                                    Personal Loan</a></li>
+                                <li><a class="" target="_blank" href="#" title="MPokket Instant Loan">MPokket Instant
+                                    Loan</a></li>
+                                {/* <!--  --> */}
+                                <li><a class="" target="_blank" href="#" title="KreditBee Personal Loan">KreditBee
+                                    Personal Loan </a></li>
+                                <li><a class="" target="_blank" href="#" title="Upward Fintech Personal Loan">Upward
+                                    Fintech Personal Loan </a></li>
+                                <li><a class="" target="_blank" href="#" title="CASHe Personal Loan">CASHe Personal Loan
+                                </a></li>
+                                <li><a class="" target="_blank" href="#" title="FlexSalary Personal Loan">FlexSalary
+                                    Personal Loan </a></li>
+                            </ul>
 
-  const getHeaderMenu = async () => {
-    const res = await axios.get('/api/headermenu');
-    const data = res.data;
-    setHeader(data)
-  }
-
-  useEffect(() => {
-    getHeaderMenu();    
-  }, [])
-
-  const items = header.map((item) => (
-    <li key={item.id} className="nav-item dropdown">
-      <a  className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
-        aria-haspopup="true" aria-expanded="false">
-        {item.name}</a>
-      <div className="flyout_Menu_container">
-        <ul className="subMenu_Container">
-          
-              {item.category.map((value)=>(
-                 <li key={value.id} className="">
-                 <a tabIndex="-1" href="#">{value.name}
-                   <span className="material-icons">
-                     chevron_right
-                   </span>
-                 </a>
-                 <ul className="">
-                  <SubMenu data={value.page} />
-                  </ul>
-               </li>  
-              ))}
-            </ul>
-         
-      </div>
-    </li>
-  ))
-
-
-
-
-  return (
-    <header className='headerWrapper'>
-      <div className="customContainer">
-        <nav className="navbar navbar-expand-lg navbar-light  navBarContainer">
-          <div className="container-fluid navContainer">
-
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-              aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <Link href="/" target="_blank" className="logoSection"><a><img
-              src="https://qa.referloan.in/assets/images/top-logo.png" alt="" title=" referloan " /></a>
-            </Link>
-
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <div className="menuBar"></div>
-              <ul className="navbar-nav">
-                {items}
-
-                <li className="nav-item"><a className="nav-link" target="_blank"
-                  href="https://qa.referloan.in/zero-investment-franchise?utm_source=direct_visitors&amp;utm_medium=self&amp;utm_campaign=&amp;utm_id=">
-                  {/* <!-- <span className="material-icons">apartment</span>  --> */}
-                  Franchise</a></li>
-
-                <li className="nav-item"><a className="nav-link" href="https://blog.referloan.in/" target="blank">
-                  {/* <!-- <span  className="material-icons">rss_feed</span>  --> */}
-                  Blog</a></li>
-                <li className="nav-item active">
-                  <Link href="/contact?utm_source=direct_visitors&amp;utm_medium=self&amp;utm_campaign=&amp;utm_id=" target="_blank" className="nav-link" >Contact
-                  </Link>
-
-                </li>
-              </ul>
+                        </li>
+                        <li> <a class="" target="_blank" href="#" title="Insurance">Insurance</a></li>
+                        <li><a class="" target="_blank" href="#" title="Investment">Investment</a></li>
+                        <li><a class="" target="_blank" href="#" title="Subsidy">Subsidy</a></li>
+                        <li><a class="" target="_blank" href="#" title="Knowledge Center">Knowledge Center</a></li>
+                        <li><a class="" target="_blank" href="#" title="About">About</a></li>
+                        <li><a class="" target="_blank" href="#" title="Franchise">Franchise</a> </li>
+                        <li><a class="" target="_blank" href="#" title="Blogs">Blogs</a> </li>
+                        <li><a class="" target="_blank" href="#" title="Contact">Contact</a> </li>
+                    </ul>
+                </nav>
             </div>
-            <div className="signInSection">
-              <a href="#">Free Cibil Score</a>
-            </div>
-          </div>
-        </nav>
 
-      </div>
-    </header>
-  )
+
+            <div class="customContainer">
+                <div class="mmenu_icon" id="mheader">
+                    <a href="#menu"><span></span></a>
+                </div>
+                <a class="logoSection"
+                    href="https://qa.referloan.in?utm_source=direct_visitors&amp;utm_medium=self&amp;utm_campaign=&amp;utm_id="><img
+                        src="https://qa.referloan.in/assets/images/top-logo.png" alt="" title=" referloan " /></a>
+
+                <div class="info_section">
+                    <ul>
+                        <li>
+                            <img src="assets/images/icon/email-icon.png" alt="" /><a
+                                href="mailto:info@referloan.in">info@referloan.in</a>
+                        </li>
+                        <li>
+                            <img src="assets/images/icon/call-icon.png" alt="" /><a href="tel:0124-4847123">0124-4847123</a>
+                        </li>
+                    </ul>
+                    <a href="#"><img src="assets/images/icon/cibil-btn.png" alt="" /></a>
+                </div>
+
+            </div>
+            <nav class=" navBarContainer">
+                <div class="container">
+                    <ul>
+                        <li>
+                            <a target="_blank" href="#" title="Cards"> Cards</a>
+
+                        </li>
+                        <li> <a class="hasSub_menu" target="_blank" href="#" title="Loan"> Loan</a>
+                            <div class="megaMenu_container">
+                                <ul>
+                                    <li> <a class="" target="_blank" href="#" title="Personal Loan">Personal Loan</a>
+                                    </li>
+                                    <li><a class="" target="_blank" href="#" title="Gold Loan">Gold Loan </a></li>
+                                    <li><a class="" target="_blank" href="#" title="Education Loan">Education Loan </a>
+                                    </li>
+                                    <li><a class="" target="_blank" href="#" title="Home Loan">Home Loan</a></li>
+                                    <li><a class="" target="_blank" href="#" title="Business Loan">Business Loan</a></li>
+                                    <li><a class="" target="_blank" href="#" title="Loan">Loan</a></li>
+                                </ul>
+                                <ul>
+                                    <li><a class="" target="_blank" href="#"
+                                        title="ICICI Bank Business Loan Overdraft">ICICI
+                                        Bank Business Loan
+                                        Overdraft</a></li>
+                                    <li><a class="" target="_blank" href="#" title="Tata Capital Used Car Loan">Tata Capital
+                                        Used Car Loan</a></li>
+                                    <li><a class="" target="_blank" href="#" title="Faircent Personal Loan">Faircent
+                                        Personal Loan</a></li>
+                                    <li><a class="" target="_blank" href="#" title="InstaMoney Personal Loan">InstaMoney
+                                        Personal Loan</a></li>
+                                    <li><a class="" target="_blank" href="#" title="MPokket Instant Loan">MPokket Instant
+                                        Loan</a></li>
+                                </ul>
+                                <ul>
+                                    <li><a class="" target="_blank" href="#" title="KreditBee Personal Loan">KreditBee
+                                        Personal Loan </a></li>
+                                    <li><a class="" target="_blank" href="#" title="Upward Fintech Personal Loan">Upward
+                                        Fintech Personal Loan </a></li>
+                                    <li><a class="" target="_blank" href="#" title="CASHe Personal Loan">CASHe Personal Loan
+                                    </a></li>
+                                    <li><a class="" target="_blank" href="#" title="FlexSalary Personal Loan">FlexSalary
+                                        Personal Loan </a></li>
+                                </ul>
+                                <div class="menu_imgBox">
+                                    <img src="assets/images/pl-bnr-icon.png" alt="" />
+                                </div>
+                            </div>
+                        </li>
+                        <li> <a class="" target="_blank" href="#" title="Insurance">Insurance</a></li>
+                        <li><a class="" target="_blank" href="#" title="Investment">Investment</a></li>
+                        <li><a class="" target="_blank" href="#" title="Subsidy">Subsidy</a></li>
+                        <li><a class="" target="_blank" href="#" title="Knowledge Center">Knowledge Center</a></li>
+                        <li><a class="" target="_blank" href="#" title="About">About</a></li>
+                        <li><a class="" target="_blank" href="#" title="Franchise">Franchise</a> </li>
+                        <li><a class="" target="_blank" href="#" title="Blogs">Blogs</a> </li>
+                        <li><a class="" target="_blank" href="#" title="Contact">Contact</a> </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Menu
