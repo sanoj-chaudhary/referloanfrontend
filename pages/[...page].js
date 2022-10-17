964a731a6871fcc395dda86db2a106e0b21d955d
priyanka agarwal <priyankaagarwal@referloan.in>
2022-10-17 21:56:31 +0530

    two level slug

--- pages/[...page].js
+++ pages/[...page].js
@@ -6,55 +6,62 @@ import Apply from './../components/page/apply';
 import MidContent from './../components/page/midcontent';
 import Head from 'next/head';
 
-function contentPage({ data }) {
-  if(data.length == 0){
-    return Error(404);
-   }else{
-    return (
-      <>
-        <Head>
-          <title>{data[0].meta_title}</title>
-          <meta name={ 'description' } content={data[0].meta_description} />
-          <meta name={ 'keywords' } content={data[0].meta_keyword} />
-        </Head>
-        
-        <Apply />
-        <div class="innerpage_bg">
-          <section class="section_pad">
-            <div class="container">
-  
-              <h1>{data[0].name}</h1>
-              <p></p>
-              <MidContent midcontent={data[0].description} />
-  
-              <div class="faqSetion">
-                <h3>FREQUENTLY ASKED QUESTIONS</h3>
-                <h2>Have a question? We've got answers!</h2>
-                <div class="faq_row">
-                  {/* FAQ List */}
-                </div>
+function contentPage({ url, data }) {
+ //console.log(url)
+  // if(data.length == 0){
+  // return Error(404);
+  //}else{
+  return (
+    <>
+      <Head>
+        <title>{data[0].meta_title}</title>
+        <meta name={'description'} content={data[0].meta_description} />
+        <meta name={'keywords'} content={data[0].meta_keyword} />
+      </Head>
+
+      <Apply />
+      <div class="innerpage_bg">
+        <section class="section_pad">
+          <div class="container">
+
+            <h1>{data[0].name}</h1>
+            <p></p>
+            <MidContent midcontent={data[0].description} />
+
+            <div class="faqSetion">
+              <h3>FREQUENTLY ASKED QUESTIONS</h3>
+              <h2>Have a question? We've got answers!</h2>
+              <div class="faq_row">
+                {/* FAQ List */}
               </div>
             </div>
-          </section>
-  
-  
-  
-        </div>
-      </>
-  
-    )
-   }
+          </div>
+        </section>
 
-  
+
+
+      </div>
+    </>
+
+  )
+  //}
+
+
 }
 
 export async function getServerSideProps(context) {
 
   let url = context.query.page;
+  let url_0 = context.query.page[0];
+  let url_1 = context.query.page[1];
 
-  const res = await db.query("SELECT * FROM `pages` WHERE `slug` = '" + url + "' ");
+  if (url_1) {
+    url = url_0 + "/" + url_1;
+  }
+  const res = await db.query("SELECT * FROM `pages` WHERE `slug` =  '" + url + "' ");
+
   const data = JSON.parse(JSON.stringify(res))
-  return { props: { data } }
+  return { props: { url, data } }
 }
 
 export default contentPage
