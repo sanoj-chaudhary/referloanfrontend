b7049ecf6a78e864c1220b41fbce18e9dbff6eb7
priyanka agarwal <priyankaagarwal@referloan.in>
2022-10-17 17:19:10 +0530

    internal pages

--- pages/[...page].js
+++ pages/[...page].js
@@ -1,23 +1,51 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import Error from "./error";
+import { db } from './../config/db'
+import Apply from './../components/page/apply';
+import Head from 'next/head';
 
 function contentPage({ data }) {
-  if (data == 0) {
-    return Error(404);
-  } else {
-    return (
-      <div className='container'
-        dangerouslySetInnerHTML={{ __html: data[0].post_content }}
-      />
-    )
-  }
+  return (
+    <>
+      <Head>
+        <title>{data[0].meta_title}</title>
+        <meta name={ 'description' } content={data[0].meta_description} />
+        <meta name={ 'keywords' } content={data[0].meta_keyword} />
+      </Head>
+      
+      <Apply />
+      <div class="innerpage_bg">
+        <section class="section_pad">
+          <div class="container">
+
+            <h1>{data[0].name}</h1>
+            <p></p>
+
+            {data[0].description}
+            
+
+            <div class="faqSetion">
+              <h3>FREQUENTLY ASKED QUESTIONS</h3>
+              <h2>Have a question? We've got answers!</h2>
+              <div class="faq_row">
+                {/* FAQ List */}
+              </div>
+            </div>
+          </div>
+        </section>
+
+
+
+      </div>
+    </>
+
+  )
 }
 
 export async function getServerSideProps(context) {
-  let url;
-  if (context.query.param[1]) {
-    url = context.query.param[1].toString();
-  } else {
-    url = context.query.param.toString();
-  }
+
+  let url = context.query.page;
 
   const res = await db.query("SELECT * FROM `pages` WHERE `slug` = '" + url + "' ");
   const data = JSON.parse(JSON.stringify(res))
