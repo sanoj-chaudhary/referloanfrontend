ca07d0c298281821493f8b5423a749dc66ab3b24
priyanka agarwal <priyankaagarwal@referloan.in>
2022-10-17 13:03:51 +0530

    footer issue resolved

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -10,16 +10,19 @@ import { useEffect, useState } from "react"
 import axios from 'axios'
 
 const Footer = () => {
-    const [menu, setMenu] = useState([]);
+    const [loanP, setLoanp] = useState([]);
+    const [loanBP, setLoanbp] = useState([]);
+    const [ccBP, setCcbp] = useState([]);
     const [loan, setLoan] = useState([]);
     const [cc, setCc] = useState([]);
-    
 
     const getFooterMenu = async () => {
         try {
             const res = await axios.get('/api/footerlink');
             const data = await res.data;
-            setMenu(data)
+            setLoanp(data.loanP[0]);
+            setLoanbp(data.loanBP[0]);
+            setCcbp(data.ccBP[0]);
         }
         catch (err) {
             console.log(err)
@@ -29,7 +32,6 @@ const Footer = () => {
     const getFooterMenu2 = async () => {
         const res = await axios.get('/api/footerlink2');
         const data = await res.data;
-        console.log(data.cc);
         setLoan(data.loan[0]);
         setCc(data.cc[0]);
     }
@@ -46,13 +48,13 @@ const Footer = () => {
             element.classList.remove('tooglrIcon');
 
         } else {
-            
+
             x.style.display = "block";
             var element = document.getElementById('footerBtn');
             element.classList.add('tooglrIcon');
         }
     }
-    
+
     return (
         <footer className='footerWrapper'>
             <div className="fot-top-bar">
@@ -67,10 +69,10 @@ const Footer = () => {
                                 <a href="mailto:helpdesk@referloan.in"> <i className="fas fa-envelope-open-text"></i> helpdesk@referloan.in</a>
                             </div>
                             <ul className="socialIcon">
-                                <li><a href="#"><i className="fab fa-facebook-f"></i></a></li>
-                                <li><a href="#"><i className="fab fa-linkedin-in"></i></a></li>
-                                <li><a href="#"><i className="fab fa-twitter"></i></a></li>
-                                <li><a href="#"><i className="fab fa-youtube"></i></a></li>
+                                <li><a href="https://www.facebook.com/referloan" target="_blank"><i className="fab fa-facebook-f"></i></a></li>
+                                <li><a href="https://www.linkedin.com/company/referloan" target="_blank"><i className="fab fa-linkedin-in"></i></a></li>
+                                <li><a href="https://twitter.com/loan_refer" target="_blank"><i className="fab fa-twitter"></i></a></li>
+                                <li><a href="https://www.youtube.com/channel/UClDi-QICJKLCQ4uLTJVsc2A" target="_blank"><i className="fab fa-youtube"></i></a></li>
                             </ul>
                         </div>
                     </div>
@@ -81,35 +83,23 @@ const Footer = () => {
                 <div className="container">
                     <div id="footerNav">
                         <div className="footerNav">
-                            {/* <ul>
-                                {menu.map((value) => (
-                                    <li key={value.id}><Link href={value.full_url} ><a><span className="material-icons">east</span>{value.post_title}</a></Link></li>
+                            <ul>
+                                {loanP.map((value) => (
+                                    <li key={value.id}><Link href={value.slug} ><a><span className="material-icons">east</span>{value.name}</a></Link></li>
                                 ))}
-                                console.log(value.full_url);
-                            </ul> */}
+                            </ul>
 
-                        <ul>
-                            <li><a href="#"><span class="material-icons">east</span> Personal Loan</a></li>
-                            <li><a href="#"><span class="material-icons">east</span> Gold Loan  </a></li>
-                            <li><a href="#"><span class="material-icons">east</span> Micro Loans</a></li>
-                            <li><a href="#"><span class="material-icons">east</span> Consumer Loan</a></li>
-                            <li><a href="#"><span class="material-icons">east</span> Business Loan</a></li>
-                            <li><a href="#"><span class="material-icons">east</span> Home loan</a></li>
-                        </ul>
-                        <ul>
-                            <li><a href="#"><span class="material-icons">east</span> Loan Against Property</a></li>
-                            <li><a href="#"><span class="material-icons">east</span> HDFC Bank Millennia Credit Card</a></li>
-                            <li><a href="#"><span class="material-icons">east</span>HDFC Bank Moneyback credit card</a></li>
-                            <li><a href="#"><span class="material-icons">east</span> HDFC Diners Club Privilege Credit Card</a></li>
-                            <li><a href="#"><span class="material-icons">east</span> Investment</a></li>
-                            <li><a href="#"><span class="material-icons">east</span> Insurance</a></li>
-                        </ul>
-                        <ul>
-                            <li><a href="#"><span class="material-icons">east</span> HDFC Bank Credit Card</a></li>
-                            <li><a href="#"><span class="material-icons">east</span> Yes Bank Credit Card</a></li>
-                            <li><a href="#"><span class="material-icons">east</span> SBI Credit Card</a></li>
-                        </ul>
+                            <ul>
+                                {loanBP.map((value) => (
+                                    <li key={value.id}><Link href={value.slug} ><a><span className="material-icons">east</span>{value.name}</a></Link></li>
+                                ))}
+                            </ul>
 
+                            <ul>
+                                {ccBP.map((value) => (
+                                    <li key={value.id}><Link href={value.slug} ><a><span className="material-icons">east</span>{value.name}</a></Link></li>
+                                ))}
+                            </ul>
 
                         </div>
                     </div>
@@ -127,35 +117,20 @@ const Footer = () => {
                         <div className="footerLinks">
                             <h2> Loan </h2>
                             <ul>
-                               {/* {loan.map((item)=>(
-                                <li key={item.id}><Link href={item.full_url} ><a  title="About">{item.post_title}</a></Link></li>
-                               ))} */}
-                            <li><a href="#" title="About">Personal Loan</a></li>
-                            <li><a href="#" title="Careers">Gold Loan</a></li>
-                            <li><a href="#" title="Blog">Micro Loan</a></li>
-                            <li><a href="#" title="Contact">Consumer Loan</a></li>
-                            <li><a href="#" title="Investors">Business Loan</a></li> 
-                            <li><a href="#" title="Investors">Home loan</a></li> 
+                                {loan.map((item)=>(
+                                    <li key={item.id}><Link href={item.slug} ><a title="{item.name}">{item.name}</a></Link></li>
+                                ))}
                             </ul>
                         </div>
 
-                        
                         <div className="footerLinks">
                             <h2>Credit Cards</h2>
                             <ul>
-                            {/* {cc.map((element)=>(
-                                <li key={element.id}><Link href={element.full_url} ><a  title="About">{element.post_title}</a></Link></li>
-                               ))} */}
-
-                            <li><a href="#" title="About">HDFC Credit Card</a></li>
-                            <li><a href="#" title="Careers">Kotak Mahindra Credit Card</a></li>
-                            <li><a href="#" title="Blog">Yes Bank Credit Card</a></li>
-                            <li><a href="#" title="Contact">ICICI Bank Credit Card</a></li>
-                            <li><a href="#" title="Investors">SBI Credit Card</a></li>
-                            <li><a href="#" title="Investors">Axis bank credit card</a></li>
+                                {cc.map((item)=>(
+                                    <li key={item.id}><Link href={item.slug} ><a title="{item.name}">{item.name}</a></Link></li>
+                                ))}
                             </ul>
                         </div>
-
 
                     </div>
                     {/* <!-- footer Card option --> */}
@@ -183,7 +158,7 @@ const Footer = () => {
                         <span>Copyright 2022 © Referloan.in.</span>
                         <span>
                             <Link href="/privacy-policy"><a title="Privacy Policy">Privacy Policy</a></Link>
-                            <Link href="/faq"><a title="Faq's">Faq's</a></Link>
+                            <Link href="/faqs"><a title="Faq's">Faq's</a></Link>
                             <Link href="/terms-and-conditions"><a title="Terms and Conditions">Terms and Conditions</a></Link>
                             <Link href="/app-privacy-policy"><a title="App Privacy Policy">App Privacy Policy</a></Link>
                             <Link href="/franchise-map"><a title="Our Franchises">Our Franchises</a></Link>
