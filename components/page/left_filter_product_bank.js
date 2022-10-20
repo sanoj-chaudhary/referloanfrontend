56ad37a27abced3bb77f6e0745e66a10dcda72e4
priyanka agarwal <priyankaagarwal@referloan.in>
2022-10-20 13:59:40 +0530

    consol log issue

--- components/page/left_filter_product_bank.js
+++ components/page/left_filter_product_bank.js
@@ -4,27 +4,27 @@ const leftfilter = () => {
   return (
     <>
 
-        <div class="filterArea">
-            <div class="inputRow">
-              <label for="">Type of Laon</label>
-              <select class="form-select" aria-label="Type of loan ">
-                <option selected>Type of loan</option>
+        <div className="filterArea">
+            <div className="inputRow">
+              <label htmlFor="">Type of Laon</label>
+              <select className="htmlForm-select" aria-label="Type of loan ">
+                <option defaultValue>Type of loan</option>
                 <option value='3'>Home Loan</option>
               </select>
             </div>
-            <div class="inputRow">
-              <label for="">Profession Type</label>
-              <select class="form-select" aria-label="Type of loan ">
-              <option selected>Profession Type</option>
+            <div className="inputRow">
+              <label htmlFor="">Profession Type</label>
+              <select className="htmlForm-select" aria-label="Type of loan ">
+              <option defaultValue>Profession Type</option>
                 <option value="1">Salaried</option>
                 <option value="2">Self employed</option>
               </select>
             </div>
-            <div class="inputRow">
-              <label for="">Pincode</label>
+            <div className="inputRow">
+              <label htmlFor="">Pincode</label>
               <input type="text" />
             </div>
-            <button class="applyBtn" title="Apply Filter">Apply Filter</button>
+            <button className="applyBtn" title="Apply Filter">Apply Filter</button>
           </div>
 
     </>
