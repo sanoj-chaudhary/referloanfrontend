4a677a9409853c5eb99e50e3fa9c77f9799aad23
sanoj-chaudhary <101033913+sanoj-chaudhary@users.noreply.github.com>
2022-10-29 17:51:36 +0530

    RF-74-add-new-header-menu

--- components/layout/header.js
+++ components/layout/header.js
@@ -43,9 +43,8 @@ export default function Header(children) {
             <link href={ '/css/global.css' } rel={ 'stylesheet' } />
             <link href={ '/css/styles.css' } rel={ 'stylesheet' } />
             <link href={ '/css/menu.css' } rel={ 'stylesheet' } />
-            <link href={ '/css/mmenu.css' } rel={ 'stylesheet' } />
-            <link href={ '/css/slick-theme.css' } rel={ 'stylesheet' } />
-            <link href={ '/css/slick.css' } rel={ 'stylesheet' } />
+            {/* <link href={ '/css/mmenu.css' } rel={ 'stylesheet' } /> */}
+            
             <link href={ '/css/media.css' } rel={ 'stylesheet' } />
             <link href={ '/css/innerPages.css' } rel={ 'stylesheet' } />
             <link href={ '/css/custom.css' } rel={ 'stylesheet' } />
