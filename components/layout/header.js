808ee4bfeb1d4d10458e740b7fa42000a0c6e837
sanoj-chaudhary <101033913+sanoj-chaudhary@users.noreply.github.com>
2022-11-09 17:56:26 +0530

    RF-204-remove-unnecessary-css

--- components/layout/header.js
+++ components/layout/header.js
@@ -40,12 +40,11 @@ export default function Header(children) {
             <link href={'/css/global.css'} rel={'stylesheet'} />
             <link href={'/css/styles.css'} rel={'stylesheet'} />
             <link href={'/css/menu.css'} rel={'stylesheet'} />
-            <link href={'/css/slick-theme.css'} rel={'stylesheet'} />
             <link href={'/css/slick.css'} rel={'stylesheet'} />
             <link href={'/css/media.css'} rel={'stylesheet'} />
             <link href={'/css/innerPages.css'} rel={'stylesheet'} />
             <link href={'/css/custom.css'} rel={'stylesheet'} />
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+            <link href={'/css/fontawesome.min.css'} rel={'stylesheet'} />
             <link href={'/css/menu-new.css'} rel={'stylesheet'} />
 
             {/* <!-- Global site tag (gtag.js) - Google Ads: 513485835 --> */}
@@ -71,8 +70,6 @@ export default function Header(children) {
                 gtag('config', 'UA-226709847-1');
             `}
             </Script>
-
-            <Script src="/js/script.js" rel="javascript" />
         </Head>
     )
 }
