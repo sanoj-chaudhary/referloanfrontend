27c31af6ca6d3232059ea91477e3c262e75e3adc
sanoj-chaudhary <101033913+sanoj-chaudhary@users.noreply.github.com>
2022-11-09 16:52:56 +0530

    RF-204-remove-unnecessary-css

--- components/layout/header.js
+++ components/layout/header.js
@@ -40,13 +40,12 @@ export default function Header(children) {
             <link href={'/css/global.css'} rel={'stylesheet'} />
             <link href={'/css/styles.css'} rel={'stylesheet'} />
             <link href={'/css/menu.css'} rel={'stylesheet'} />
-            <link href={'/css/mmenu.css'} rel={'stylesheet'} />
             <link href={'/css/slick-theme.css'} rel={'stylesheet'} />
             <link href={'/css/slick.css'} rel={'stylesheet'} />
             <link href={'/css/media.css'} rel={'stylesheet'} />
             <link href={'/css/innerPages.css'} rel={'stylesheet'} />
             <link href={'/css/custom.css'} rel={'stylesheet'} />
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
             <link href={'/css/menu-new.css'} rel={'stylesheet'} />
 
             {/* <!-- Global site tag (gtag.js) - Google Ads: 513485835 --> */}
