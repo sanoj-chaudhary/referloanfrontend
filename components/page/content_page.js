65414f8032fb340141ca14ad2e04ff0cbc2606e7
priyanka agarwal <priyankaagarwal@referloan.in>
2022-11-30 10:46:50 +0530

    set faq div in container

--- components/page/content_page.js
+++ components/page/content_page.js
@@ -13,56 +13,61 @@ const midcontent = ({ data, faq }) => {
       {loading && <Loader />}
       <div className="innerpage_bg">
         <section className="section_pad">
+
           <div className="container">
             <div dangerouslySetInnerHTML={{ __html: data[0].description }}></div>
-            <div className="table_with_img_content">
+          </div>
 
+          <div className="container">
             {faq != '' ? <div className="faqSetion" itemscope="true" itemtype="https://schema.org/FAQPage">
-                <h3>FREQUENTLY ASKED QUESTIONS</h3>
-                <h2>Have a question? We've got answers!</h2>
-                <div className="faq_row">
-                  <div
-                    className="accordion accordion-flush faqAccordion "
-                    id="accordionFlushExample"
-                  >
-                    {faq.map((item, key) => (
-                      <div key={key} className="accordion-item" itemscope="true" itemprop="mainEntity" itemtype="https://schema.org/Question">
-                        <h2 className="accordion-header" id={"flush-heading" + key} itemprop="name">
-                          <button
-                            className="accordion-button collapsed"
-                            type="button"
-                            data-bs-toggle="collapse"
-                            data-bs-target={"#flush-collapse" + key}
-                            aria-expanded="false"
-                            aria-controls={"flush-collapse" + key}
-                          >
-                            {item.question}
-                          </button>
-                        </h2>
-                        <div
-                          id={"flush-collapse" + key}
-                          className="accordion-collapse collapse"
-                          aria-labelledby="flush-headingTwo"
-                          data-bs-parent="#accordionFlushExample" 
+              <h3>FREQUENTLY ASKED QUESTIONS</h3>
+              <h2>Have a question? We've got answers!</h2>
+              <div className="faq_row">
+                <div
+                  className="accordion accordion-flush faqAccordion "
+                  id="accordionFlushExample"
+                >
+                  {faq.map((item, key) => (
+                    <div key={key} className="accordion-item" itemscope="true" itemprop="mainEntity" itemtype="https://schema.org/Question">
+                      <h2 className="accordion-header" id={"flush-heading" + key} itemprop="name">
+                        <button
+                          className="accordion-button collapsed"
+                          type="button"
+                          data-bs-toggle="collapse"
+                          data-bs-target={"#flush-collapse" + key}
+                          aria-expanded="false"
+                          aria-controls={"flush-collapse" + key}
                         >
-                          <div className="accordion-body" itemscope="true" itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
-                            <div  itemprop="text" dangerouslySetInnerHTML={{ __html: item.answer }}></div>
-                          </div>
+                          {item.question}
+                        </button>
+                      </h2>
+                      <div
+                        id={"flush-collapse" + key}
+                        className="accordion-collapse collapse"
+                        aria-labelledby="flush-headingTwo"
+                        data-bs-parent="#accordionFlushExample" 
+                      >
+                        <div className="accordion-body" itemscope="true" itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                          <div  itemprop="text" dangerouslySetInnerHTML={{ __html: item.answer }}></div>
                         </div>
                       </div>
-                    ))}
-                  </div>
-
-                  <div className="faqImg">
-                    <img src="/images/faq.webp" alt="" />
-                  </div>
+                    </div>
+                  ))}
                 </div>
-              </div>:''}
-              
-            </div>
+
+                <div className="faqImg">
+                  <img src="/images/faq.webp" alt="" />
+                </div>
+              </div>
+            </div>:''}
           </div>
+
         </section>
       </div>
+
+      <section>
+         
+      </section>
     </>
   );
 };
