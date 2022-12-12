a06937fa4525cd0f0e63f758a42af21df6ce247d
sanoj-chaudhary <101033913+sanoj-chaudhary@users.noreply.github.com>
2022-12-12 22:02:48 +0530

    Merge branch 'develop' of https://bitbucket.org/hydear/referloan-frontend into RF-306-correct-the-rating-in-apply-secti
    # Conflicts:
    #	components/page/rating.js

--- components/page/rating.js
+++ components/page/rating.js
@@ -58,7 +58,6 @@ const StarRating = ({ data,ratinginfo1 }) => {
           {[...Array(5)].map((star, index) => {
             index += 1;
             return (
-
               <button
                 type="button"
                 key={index}
