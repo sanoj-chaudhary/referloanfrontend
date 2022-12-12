1035a8052c2c76b96e5d95d87a7a85ac79558ac2
Satish Chandra <satish@hydear.net.in>
2022-12-12 12:50:21 +0000

    Revert "RF-304 add a redirecting message with fa (pull request #586)"

--- components/page/generateOtp.js
+++ components/page/generateOtp.js
@@ -44,8 +44,7 @@ const GenerateOtp = ({ setToken, setUserValues, data, setServerSideMsg, setServe
     setServerSideStatus(true)
     try {
 
-      const { otp } = values;
-      const { phone_no } = genOtpData;
+      const { phone_no, otp } = values;
       const data = {
         phone_no, otp, bank_product_id, utm_campaign, utm_id: utmId, utm_medium, utm_source, offer: ""
       }
