9bfab5e2fc7227703f3c24e22cc583c25e0e0009
sanoj-chaudhary <101033913+sanoj-chaudhary@users.noreply.github.com>
2022-12-19 19:22:51 +0530

    RF-320-add-marker-in-franchise-map-segme

--- components/page/callbackForm.js
+++ components/page/callbackForm.js
@@ -4,8 +4,8 @@ import { useState } from "react";
 import * as Yup from "yup";
 
 const callbackForm = () => {
-const [error, setError] = useState(false)
-const [message, setMessage] = useState()
+  const [error, setError] = useState(false)
+  const [message, setMessage] = useState()
   const initialValues = {
     name: '',
     email: '',
@@ -14,7 +14,7 @@ const [message, setMessage] = useState()
 
   const callbackSchema = Yup.object({
     name: Yup.string().min(2, 'Invalid name').required("Please enter your name "),
-    email:Yup.string().email(),
+    email: Yup.string().email(),
     phone_no: Yup.string().min(10, 'Invalid phone number').max(10, 'Invalid phone number').required("Please enter your phone number").matches(/^\+?[6-9][0-9]{7,14}$/, "Invalid phone number"),
   });
 
@@ -24,13 +24,13 @@ const [message, setMessage] = useState()
       validationSchema: callbackSchema,
       onSubmit: async (values, action) => {
         try {
-         const res = await axios.post(`${process.env.APIHOST}/api/franchise_enquiry`, values);
-         if(res.data.status){
-          setError(false)
-          setMessage('Thank you for enquiry')
-         }else{
-          setError(false)
-         }
+          const res = await axios.post(`${process.env.APIHOST}/api/franchise_enquiry`, values);
+          if (res.data.status) {
+            setError(false)
+            setMessage('Thank you for enquiry')
+          } else {
+            setError(false)
+          }
         } catch (error) {
           setError(true)
           console.log("Message : ", error.message)
@@ -39,35 +39,57 @@ const [message, setMessage] = useState()
     });
 
   return (
-    <form onSubmit={(e)=>{ e.preventDefault(),  handleSubmit()}}>
-      <div className="form-floating mb-3">
-        <input type="text" name="name" autoComplete="off" className="form-control shadow-none" required onChange={handleChange} value={values.name} />
+    <>
+      <div className="heading">Let's start something big together.</div>
+      <p>ReferLoan introduced a franchise model which allows you to grow your business at zero investment and
+        risk-free life.</p>
+      <div className="feature_area">
+        <h3>ReferLoan - A leading fintech company</h3>
+        <ul>
+          <li>
+            <span className="icon_box">345+</span>
+            <h2>Financial Products</h2>
+          </li>
+          <li>
+            <span className="icon_box">175+</span>
+            <h2>Banks/NBFCs tie-ups</h2>
+          </li>
+          <li>
+            <span className="icon_box">40+</span>
+            <h2>Franchise Openings</h2>
+          </li>
+        </ul>
+      </div>
+      <form onSubmit={(e) => { e.preventDefault(), handleSubmit() }}>
+        <div className="form-floating mb-3">
+          <input type="text" name="name" autoComplete="off" className="form-control shadow-none" required onChange={handleChange} value={values.name} />
 
-        <label htmlFor="floatingInput">Name</label>
-        {errors.name && touched.name ? (
+          <label htmlFor="floatingInput">Name</label>
+          {errors.name && touched.name ? (
             <p className="form-error">{errors.name}</p>
           ) : null}
-      </div>
-      <div className="form-floating mb-3">
-        <input type="email" name="email" autoComplete="off"    className="form-control shadow-none" required onChange={handleChange} value={values.email} />
-        <label htmlFor="floatingInput">Email</label>
-        {errors.email && touched.email ? (
+        </div>
+        <div className="form-floating mb-3">
+          <input type="email" name="email" autoComplete="off" className="form-control shadow-none" required onChange={handleChange} value={values.email} />
+          <label htmlFor="floatingInput">Email</label>
+          {errors.email && touched.email ? (
             <p className="form-error">{errors.email}</p>
           ) : null}
-      </div>
-      <div className="form-floating mb-3">
-        <input type="number" name="phone_no" autoComplete="off" className="form-control shadow-none" maxLength="10" required onChange={handleChange} value={values.phone_no} />
-        <label htmlFor="floatingInput">Mobile No</label>
-        {errors.phone_no && touched.phone_no ? (
+        </div>
+        <div className="form-floating mb-3">
+          <input type="number" name="phone_no" autoComplete="off" className="form-control shadow-none" maxLength="10" required onChange={handleChange} value={values.phone_no} />
+          <label htmlFor="floatingInput">Mobile No</label>
+          {errors.phone_no && touched.phone_no ? (
             <p className="form-error">{errors.phone_no}</p>
           ) : null}
-      </div>
-      <span className="text-success">{message}</span>
-      {error && <span className="text-danger">Something went wrong!</span>}
-      
-       <div className="search-button"><button className="my-2" type="submit">Let’s Go!</button></div>
+        </div>
+        <span className="text-success">{message}</span>
+        {error && <span className="text-danger">Something went wrong!</span>}
 
-    </form>
+        <div className="search-button"><button className="my-2" type="submit">Let’s Go!</button></div>
+
+      </form>
+    </>
   )
 }
 
--- pages/franchise-map.js
+++ pages/franchise-map.js
@@ -1,24 +1,21 @@
 import CallbackForm from "../components/page/callbackForm"
-import { useLoadScript } from "@react-google-maps/api"
 import Map from './../components/page/map'
 
 const Franchisemap = () => {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: "AIzaSyC5vJg6Zs-nho_xeQgpVJXJsm8rWN1wovU" // Add your API key
-  });
+
   return (
     <>
   
-      <div class="franchise-banner">
+      <div className="franchise-banner">
         <img src="/images/fran-banner.jpg" alt="" />
       </div>
-      <h1 class="text-center"> Our Franchise Maps</h1>
-    <div className="container">
-      <div className="row">
+      <h1 className="text-center"> Our Franchise Maps</h1>
+    <div className="container-fluid mb-4 px-4">
+      <div className="row map_container">
         <div className="col-md-6">
         <Map />
         </div>
-        <div className="col-md-6">
+        <div className="col-md-6 franchise_listing_area ">
           <CallbackForm />
         </div>
       </div>
